9e320e30a2ad2968ea97ed6d81bacea72403a811
whyoleg <whyoleg@gmail.com>
2019-10-12 02:03:28 +0300

    update bootstrap implementation
    publish 0.1.11

--- library/src/main/kotlin/dev/whyoleg/kamp/settings/ExperimentalAnnotation.kt
+++ library/src/main/kotlin/dev/whyoleg/kamp/settings/ExperimentalAnnotation.kt
@@ -8,6 +8,7 @@ enum class ExperimentalAnnotation(val value: String) {
     ImplicitReflectionSerializer("kotlinx.serialization.ImplicitReflectionSerializer"),
     KtorExperimentalAPI("io.ktor.util.KtorExperimentalAPI"),
     Experimental("kotlin.Experimental"),
+    ExperimentalStdlibApi("kotlin.ExperimentalStdlibApi"),
     ExperimentalUnsignedTypes("kotlin.ExperimentalUnsignedTypes"),
     ExperimentalContracts("kotlin.contracts.ExperimentalContracts"),
     ExperimentalTime("kotlin.time.ExperimentalTime")
