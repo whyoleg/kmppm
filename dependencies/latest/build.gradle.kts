e04a1cb70797c8f0926913119855cc11ad278081
whyoleg <whyoleg@gmail.com>
2019-10-12 14:55:49 +0300

    fix

--- dependencies/latest/build.gradle.kts
+++ dependencies/latest/build.gradle.kts
@@ -1,3 +1,9 @@
+import dev.whyoleg.kamp.*
+import dev.whyoleg.kamp.dependency.classifier.*
+import dev.whyoleg.kamp.plugin.*
+import dev.whyoleg.kamp.sourceset.*
+import dev.whyoleg.kamp.target.*
+
 repositories {
     val eapProviders: List<DependencyProviderClassifier> = listOf(object : KotlinEapProviderClassifier {})
     eapProviders.forEach { it.provider(this) }
--- dependencies/stable/build.gradle.kts
+++ dependencies/stable/build.gradle.kts
@@ -1,3 +1,8 @@
+import dev.whyoleg.kamp.*
+import dev.whyoleg.kamp.plugin.*
+import dev.whyoleg.kamp.sourceset.*
+import dev.whyoleg.kamp.target.*
+
 kampJvm(versionsKind = "stable") {
     plugins(BuiltInPlugins.updates)
     source(properties["collectedDependencies"] as SourceSetBuilder<JvmTarget>.() -> Unit)
