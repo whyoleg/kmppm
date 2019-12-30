752dbf65f2923535615116daf66c4c0a31dc2475
whyoleg <whyoleg@gmail.com>
2019-12-31 00:18:01 +0300

    add project dependencies

--- plugin/src/main/kotlin/test.kt
+++ plugin/src/main/kotlin/test.kt
@@ -1,11 +1,15 @@
 import dev.whyoleg.kamp.dependencies.*
+import dev.whyoleg.kamp.dependency.*
 import dev.whyoleg.kamp.platform.*
 import org.jetbrains.kotlin.gradle.dsl.*
 
 fun KotlinMultiplatformExtension.test() {
     val kotlin = KotlinModule(KotlinVersion.stable)
+    val apiProject = KampProjectDependency("123")
     listOf(jvm(), js()).dependencies("main") {
+        implementation(apiProject) {
 
+        }
     }
     listOf(jvm(), js()).dependenciesMain {
         with(kotlin.dependencies) {
